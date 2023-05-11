ac78196c57c5510991e07e05763a72ee55ff6a44
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-05-11 20:11:50 +0800

    feat: serialize to Item

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use csv;
+use serde::{Deserialize, Serialize};
 
 use std::io;
 
+#[derive(Debug, Deserialize, Serialize)]
+struct Item {
+    #[serde(rename = "Item Name")]
+    name: String,
+    #[serde(rename = "Rate")]
+    price: String,
+    #[serde(rename = "Purchase Rate")]
+    cost: String,
+}
+
 fn main() {
     let mut reader = csv::Reader::from_reader(io::stdin());
-    for result in reader.records() {
-        let record = result.expect("a CSV record");
+    for result in reader.deserialize() {
+        let record: Item = result.expect("a CSV record");
         println!("{:?}", record);
     }
 }
