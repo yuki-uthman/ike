acb8ac3bfd764304988378627c9f572f848207c1
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-05-11 19:57:27 +0800

    feat: read item.csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use csv;
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut reader = csv::Reader::from_reader(io::stdin());
+    for result in reader.records() {
+        let record = result.expect("a CSV record");
+        println!("{:?}", record);
+    }
 }
