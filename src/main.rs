138b260c8ce87aa7c602f3547f25308ae3881c90
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-05-15 19:38:53 +0800

    chor: remove unused code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 use shop::records::Records;
 use shop::invoices::Invoices;
 
-fn main() {
-    println!("{:#?}", invoices)
-    let invoices = Invoices::load("assets/Invoice.csv").unwrap();
-}
+fn main() {}
