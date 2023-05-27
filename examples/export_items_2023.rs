670d01d5d9a7f81782c2a7e0e94bec84b0e445f2
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-05-27 16:13:16 +0800

    docs: update export items

--- examples/export_items_2023.rs
+++ examples/export_items_2023.rs
@@ -8,21 +8,26 @@ fn main() -> Result<(), Error> {
         filename: "assets/zoho/Item.csv",
         source,
     })?;
-    let start = NaiveDate::from_ymd_opt(2023, 1, 1).unwrap();
+    let start = NaiveDate::from_ymd_opt(2021, 1, 1).unwrap();
     let end = NaiveDate::from_ymd_opt(2023, 6, 1).unwrap();
-    let items = invoices.between(start, end).unique();
+    let items = invoices.between(start, end).unique_items();
 
-    let filename = "examples/output/2023.csv";
+    let filename = "examples/output/items.csv";
 
     std::fs::File::create(filename).unwrap();
 
     let mut writer = csv::Writer::from_path(filename).unwrap();
-    writer.write_record(&["Date", "Name", "Quantity"]).unwrap();
+    writer.write_record(&["id", "name"]).unwrap();
 
+    let mut id = 1;
     for item in items.iter() {
+        if item.name().is_empty() {
+            continue;
+        }
         writer
-            .write_record(&["01/05/2023", &item.name(), "0"])
+            .write_record(&[id.to_string(), item.name().to_string()])
             .unwrap();
+        id += 1;
     }
     writer.flush().unwrap();
 
