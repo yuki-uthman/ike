098b7577c3478512fb625e0c4654df562392eb80
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-05-30 12:49:03 +0800

    docs: update unique() to unique_names()

--- examples/deprecated_items.rs
+++ examples/deprecated_items.rs
@@ -11,11 +11,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let start = NaiveDate::from_ymd_opt(2022, 1, 1).unwrap();
     let end = NaiveDate::from_ymd_opt(2022, 12, 31).unwrap();
-    let items_2022 = invoices.between(start, end).unique();
+    let items_2022 = invoices.between(start, end).unique_items();
 
     let start = NaiveDate::from_ymd_opt(2023, 1, 1).unwrap();
     let end = NaiveDate::from_ymd_opt(2023, 6, 1).unwrap();
-    let items_2023 = invoices.between(start, end).unique();
+    let items_2023 = invoices.between(start, end).unique_items();
 
     let deprecated = items_2022
         .iter()
