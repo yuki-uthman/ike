fb1f15134c8b102ee42ec0573ba2ae937fe3d78f
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-06-11 14:05:49 +0800

    feat: use Items.find_all to match item

--- src/bin/last_sold.rs
+++ src/bin/last_sold.rs
@@ -1,16 +1,49 @@
+use chrono::Utc;
 use colored::Colorize;
 use shop::Invoices;
+use shop::Items;
 use shop::Loader;
 
 pub fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let item_name = &args[1];
+    let pattern = &args[1];
+
+    let items = Items::load_from_file("assets/Item.csv").unwrap();
+    let items = items.find_all(pattern).unwrap();
 
     let invoices = Invoices::load_from_file("assets/Invoice.csv").unwrap();
 
-    let option = invoices.last_sold(item_name);
-    match option {
-        Some(date) => println!("{}: {}", item_name, date.to_string().green().bold()),
-        None => println!("{}: {}", item_name, "Never sold".red().bold()),
+    let today = Utc::now().naive_utc().date();
+    let mut vec = items
+        .iter()
+        .map(|item| {
+            let option = invoices.last_sold(item.name());
+            let num_of_days = if let Some(date) = option {
+                (today - date).num_days()
+            } else {
+                9999
+            };
+            (num_of_days, item)
+        })
+        .collect::<Vec<_>>();
+    vec.sort_by(|a, b| a.0.cmp(&b.0));
+
+    for item in vec {
+        let num_of_days = item.0.to_string();
+        if item.0 == 9999 {
+            println!(
+                "{:>5} {}: {}",
+                "~~~".red().bold(),
+                "days ago".red().bold(),
+                item.1.name().red().bold()
+            );
+        } else {
+            println!(
+                "{:>5} {}: {}",
+                num_of_days.green().bold(),
+                "days ago".green().bold(),
+                item.1.name().green().bold()
+            );
+        }
     }
 }
