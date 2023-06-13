6cb1b608579e4bd6103c9751c3bd4dbd8a229d85
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-06-13 13:55:06 +0800

    feat: add sieve bin

---
+++ src/bin/sieve.rs
@@ -0,0 +1,34 @@
+use colored::Colorize;
+use shop::Items;
+use shop::Loader;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub fn main() -> Result<()> {
+    let items = Items::load_from_file("assets/Item.csv")?.only_active_items();
+
+    let args: Vec<String> = std::env::args().collect();
+
+    let first_pattern = &args[1];
+    let mut main_items = items.find_all(first_pattern)?;
+
+    for arg in args.iter().skip(2) {
+        let to_remove = items.find_all(arg)?;
+        main_items = main_items - to_remove;
+    }
+
+    if main_items.is_empty() {
+        println!();
+        println!("    {}", "All Sieved!".green().bold());
+        println!();
+        return Ok(());
+    }
+
+    println!();
+    for item in main_items.iter() {
+        println!("     {}", item.name().red().bold());
+    }
+    println!();
+
+    Ok(())
+}
