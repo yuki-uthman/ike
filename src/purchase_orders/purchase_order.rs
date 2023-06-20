dc1c3c08b9e5476763ff60faa5adf5f4a12ab43d
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-06-20 17:22:41 +0800

    feat: deserialize Purchase Order date to NaiveDate

--- src/purchase_orders/purchase_order.rs
+++ src/purchase_orders/purchase_order.rs
@@ -1,13 +1,22 @@
-use serde::{Deserialize, Serialize};
+use chrono::NaiveDate as Date;
+use serde::Deserialize;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize)]
 pub struct PurchaseOrder {
     #[serde(rename = "Purchase Order Status")]
     status: String,
-    #[serde(rename = "Purchase Order Date")]
-    date: String,
+    #[serde(rename = "Purchase Order Date", deserialize_with = "deserialize_date")]
+    date: Date,
     #[serde(rename = "Item Name")]
     item_name: String,
     #[serde(rename = "QuantityOrdered")]
     quantity: usize,
+}
+
+fn deserialize_date<'de, D>(deserializer: D) -> std::result::Result<Date, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let string = String::deserialize(deserializer)?;
+    Ok(Date::parse_from_str(&string, "%Y-%m-%d").map_err(serde::de::Error::custom)?)
 }
