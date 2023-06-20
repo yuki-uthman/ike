78e59d2ce1a70c4e3d04c80a101374984b0dd990
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-06-20 17:11:22 +0800

    feat: read Purchase_Order.csv

--- src/purchase_orders/purchase_order.rs
+++ src/purchase_orders/purchase_order.rs
@@ -1,5 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct PurchaseOrder {
-    id: String,
+    #[serde(rename = "Purchase Order Status")]
+    status: String,
+    #[serde(rename = "Purchase Order Date")]
     date: String,
-    items: String,
+    #[serde(rename = "Item Name")]
+    item_name: String,
+    #[serde(rename = "QuantityOrdered")]
+    quantity: usize,
 }
--- src/purchase_orders/purchase_orders.rs
+++ src/purchase_orders/purchase_orders.rs
@@ -1,3 +1,37 @@
 use super::PurchaseOrder;
+use crate::loader::Loader;
+use std::ops::Deref;
 
+#[derive(Debug)]
 pub struct PurchaseOrders(Vec<PurchaseOrder>);
+
+impl Loader<PurchaseOrder> for PurchaseOrders {}
+
+impl From<Vec<PurchaseOrder>> for PurchaseOrders {
+    fn from(vec: Vec<PurchaseOrder>) -> PurchaseOrders {
+        PurchaseOrders(vec)
+    }
+}
+
+impl Deref for PurchaseOrders {
+    type Target = Vec<PurchaseOrder>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use insta::assert_yaml_snapshot;
+
+    #[test]
+    fn len() {
+        let po = PurchaseOrders::load_from_file("assets/Purchase_Order.csv").unwrap();
+        assert_yaml_snapshot!(po.len(), @r###"
+        ---
+        444
+        "###);
+    }
+}
