ff5a24ff25ff362fca9214c6f4c1371598085a28
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-06-22 11:03:12 +0800

    feat: add PurchaseOrder Status enum

--- src/purchase_orders/purchase_order.rs
+++ src/purchase_orders/purchase_order.rs
@@ -1,10 +1,17 @@
 use chrono::NaiveDate as Date;
 use serde::Deserialize;
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Status {
+    Draft,
+    Billed,
+    Issued,
+}
+
 #[derive(Clone, Debug, Deserialize)]
 pub struct PurchaseOrder {
-    #[serde(rename = "Purchase Order Status")]
-    status: String,
+    #[serde(rename = "Purchase Order Status", deserialize_with = "deserialize_status")]
+    status: Status,
     #[serde(rename = "Purchase Order Date", deserialize_with = "deserialize_date")]
     date: Date,
     #[serde(rename = "Item Name")]
@@ -21,9 +28,25 @@ where
     Ok(Date::parse_from_str(&string, "%Y-%m-%d").map_err(serde::de::Error::custom)?)
 }
 
+fn deserialize_status<'de, D>(deserializer: D) -> std::result::Result<Status, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let status = String::deserialize(deserializer).unwrap();
+    match status.as_str() {
+        "Draft" => Ok(Status::Draft),
+        "Billed" => Ok(Status::Billed),
+        "Issued" => Ok(Status::Issued),
+        _ => {
+            let msg = format!("invalid status: {}", status);
+            Err(serde::de::Error::custom(msg))
+        }
+    }
+}
+
 impl PurchaseOrder {
-    pub fn status(&self) -> String {
-        self.status.clone()
+    pub fn status(&self) -> Status {
+        self.status
     }
 
     pub fn date(&self) -> Date {
