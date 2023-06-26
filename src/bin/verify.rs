2e22936a65c8a49b84381a730abf686c940c0a6f
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-06-26 17:02:49 +0800

    feat: add verify binary to check Tax field

---
+++ src/bin/verify.rs
@@ -0,0 +1,28 @@
+use serde::de::DeserializeOwned;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+use colored::Colorize;
+use shop::Items;
+use shop::ItemTaxName;
+
+use shop::Loader;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub fn main() -> Result<()>{
+    let items = Items::load_from_file("assets/Item.csv")?;
+    let mut items = items.filter(|item| item.tax_name() == ItemTaxName::None);
+    items.sort();
+
+    println!();
+    println!("  {}", "Tax Field Missing!!!".red().bold());
+    for item in items.iter() {
+        println!("     {}", item.name().green().bold());
+    }
+    println!();
+
+    Ok(())
+}
