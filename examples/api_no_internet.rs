4104bc0b8457c359b2b700700e0885017ec8d22a
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-07-12 05:02:15 +0800

    docs: add example to check no internet error

---
+++ examples/api_no_internet.rs
@@ -0,0 +1,11 @@
+use shop::Api;
+
+#[tokio::main]
+async fn main() {
+    let api = Api::new("credentials".to_string());
+
+    // turn off internet and run this example
+    // make sure it returns Error
+    println!("{:#?}", api);
+}
+
