587c5160763e08d3adc899d323763d7f552a6866
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-07-13 11:14:06 +0800

    docs: update api_init

--- examples/api_init.rs
+++ examples/api_init.rs
@@ -1,12 +1,12 @@
-use shop::Client;
+use shop::api::Client;
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = std::env::args().collect();
     let code = &args[1];
 
-    let client = Client::read_from("client.json").unwrap();
+    let client = Client::read_from("credentials/client.json").unwrap();
     let token = client.get_initial_token(code).await.unwrap();
-    token.write_to("token.json").unwrap();
+    // token.write_to("credentials/token.json").unwrap();
 }
 
