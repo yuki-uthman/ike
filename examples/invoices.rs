04f0499ee1092b287297440ba23ba99c7aa4458e
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-08-26 17:32:46 +0800

    docs: accept argument to filter the item

--- examples/invoices.rs
+++ examples/invoices.rs
@@ -3,9 +3,14 @@ use shop::Invoices;
 use shop::Loader;
 
 pub fn main() {
+
+    let item_name = std::env::args().nth(1).unwrap_or_else(|| {
+        println!("Usage: invoices <item name>");
+        std::process::exit(1);
+    });
+
     let invoices = Invoices::load_from_file("assets/Invoice.csv").unwrap();
 
-    let item_name = "Round Container R12 370Ml 50*10Pcs";
     let sales = invoices
         .iter()
         .filter(|invoice| invoice.item_name() == item_name)
@@ -14,11 +19,16 @@ pub fn main() {
     let launch = sales.first().unwrap().date();
     let today = chrono::Local::now().date_naive();
 
+    println!();
+    println!(
+        "   {}",
+        item_name.green().bold()
+    );
     // iterate from launch date to today
     for date in launch.iter_days() {
-        let orders_count = invoices.on(date).count_frequency(item_name);
+        let orders_count = invoices.on(date).count_frequency(&item_name);
         println!(
-            "{}: {} {}",
+            "     {}: {} {}",
             date.to_string(),
             orders_count.to_string().green().bold(),
             "times ordered".green()
