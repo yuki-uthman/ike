dd458d7a21a7a523ee0d516f28b5a29b0cf8de44
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-08-26 19:05:51 +0800

    feat: add sold binary

---
+++ src/bin/sold.rs
@@ -0,0 +1,66 @@
+use colored::Colorize;
+use shop::Invoices;
+use shop::Items;
+use shop::Loader;
+
+pub fn main() {
+    let item_name = std::env::args().nth(1).unwrap_or_else(|| {
+        println!("Usage: invoices <item name>");
+        std::process::exit(1);
+    });
+
+    let invoices = Invoices::load_from_file("assets/Invoice.csv")
+        .unwrap()
+        .get_closed();
+
+    let items = Items::load_from_file("assets/Item.csv").unwrap();
+    let matches = items.find_all(&item_name).unwrap();
+
+    for item in matches.iter() {
+        let sales = invoices
+            .iter()
+            .filter(|invoice| invoice.item_name() == item.name())
+            .collect::<Vec<_>>();
+
+        if sales.len() == 0 {
+            continue;
+        }
+
+        let launch = sales.first().unwrap().date();
+        let today = chrono::Local::now().date_naive();
+
+        println!();
+        println!("   {}", item.name().green().bold());
+
+        let mut total = 0;
+        for date in launch.iter_days() {
+            let orders_count = invoices.on(date).count_quantity_sold(&item.name());
+            total += orders_count;
+
+            if orders_count == 0 {
+                if date > today {
+                    break;
+                }
+                continue;
+            }
+
+            println!(
+                "     {}: {} {}",
+                date.to_string(),
+                orders_count.to_string().green().bold(),
+                "pcs sold".green()
+            );
+
+            if date > today {
+                break;
+            }
+        }
+        println!(
+            "     {}      {}",
+            "Total:".green().bold(),
+            total.to_string().green().bold()
+        );
+        println!();
+    }
+}
+
