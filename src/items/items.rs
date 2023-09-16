e9dd8dafadbda31add7096b9213500d6416b7246
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-09-16 23:57:45 +0800

    feat: add method to get counted items

--- src/items/items.rs
+++ src/items/items.rs
@@ -182,6 +182,14 @@ impl Items {
         unique_items.into()
     }
 
+    pub fn get_counted_items(&self) -> Self {
+        self.iter()
+            .filter(|item| item.is_counted())
+            .map(|item| item.clone())
+            .collect::<Vec<Item>>()
+            .into()
+    }
+
     pub fn add(&mut self, item: Item) {
         self.0.push(item);
     }
