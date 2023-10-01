9f487e9f10f374c76ff931eb3071b702b5b718f8
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-10-01 23:47:08 +0800

    docs: export profit to file

--- examples/invoice_profit.rs
+++ examples/invoice_profit.rs
@@ -1,6 +1,17 @@
+use std::fs::File;
+
+use chrono::NaiveDate as Date;
 use colored::Colorize;
+use serde::Deserialize;
+use serde::Serialize;
 use shop::Invoices;
 use shop::Loader;
+
+#[derive(Debug, Clone, PartialEq)]
+struct Profit {
+    date: Date,
+    profit: i32,
+}
 
 pub fn main() {
     let mut invoices = Invoices::load_from_file("assets/Invoice.csv")
@@ -9,16 +20,45 @@ pub fn main() {
 
     invoices.inject_items();
 
+    invoices.remove_by_item_name("TEAK PVC GLOSSY 0.5MM(960) X 22MM");
+
     // September
-    let start = chrono::NaiveDate::from_ymd(2023, 9, 1);
-    let end = chrono::NaiveDate::from_ymd(2023, 9, 28);
+    let start = chrono::NaiveDate::from_ymd_opt(2023, 9, 1);
+    let end = chrono::NaiveDate::from_ymd_opt(2023, 9, 28);
 
-    for day in start.iter_days() {
-        let profit = invoices.on(day).count_profit() as i32;
-        println!("{}: {:>8}", day.format("%Y-%m-%d"), profit.to_string().green());
+    let mut profits = Vec::new();
+    for date in start.iter_days() {
+        let profit = invoices.on(date).count_profit() as i32;
 
-        if day == end {
+        profits.push(Profit { date, profit });
+
+        println!(
+            "{}: {:>8}",
+            date.format("%Y-%m-%d"),
+            profit.to_string().green()
+        );
+
+        if date == end {
             break;
         }
     }
+
+    let filename = "examples/output/profit.csv";
+    File::create(&filename).unwrap();
+    let mut writer = csv::Writer::from_path(filename).unwrap();
+
+    writer
+        .write_record(&["Day", "Profit"])
+        .unwrap();
+
+    for profit in profits.iter() {
+        writer
+            .write_record(&[
+                profit.date.format("%d").to_string(),
+                profit.profit.to_string(),
+            ])
+            .unwrap();
+    }
+
+    writer.flush().unwrap();
 }
