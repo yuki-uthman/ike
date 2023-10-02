2dde90a66b19e7418b38bb22ec1dcf0bfc91b8a2
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-10-03 01:51:30 +0800

    test: update test

--- src/invoices/invoices.rs
+++ src/invoices/invoices.rs
@@ -255,7 +255,7 @@ mod tests {
         let invoices = Invoices::load_from_file("assets/Invoice.csv").unwrap();
         assert_yaml_snapshot!(invoices.len(), @r###"
         ---
-        7826
+        7915
         "###);
     }
 
